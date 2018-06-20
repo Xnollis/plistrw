0c76fa8f6f69ab619e8261a6e3bddd9c4e4371ba
Xnollis <jinx@longjubank.com>
2018-06-20 17:16:08 +0800

    complete test running...

--- plistrw/main.m
+++ plistrw/main.m
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[])
         fprintf(stdout,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict/></plist>");
         return 0;
     }
-    if(argc<3)
+    if(argc<3&&bIsRunTestMode==NO)
     {
         fprintf(stderr,"Invalid params.\n");
         print_usage();
